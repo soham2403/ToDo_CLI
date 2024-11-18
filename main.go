28dec8171cf2bebc899c736cd746ffd146d643cf
soham2403 <sohammore2403@gmail.com>
2024-11-18 12:35:56 +0530

    added 2 new methods

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	tds := todos{}
 	tds.add("learn go")
 	tds.add("uiasgfi")
-	fmt.Println("main to do app")
-	fmt.Println(tds)
+	tds.toogle(0)
+	tds.print()
 }
--- todo.go
+++ todo.go
@@ -3,7 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/aquasecurity/table"
 )
 
 type todo struct {
@@ -42,4 +46,50 @@ func (tds *todos) delete(id int) error {
 
 	*tds = append(t[:id], t[id+1:]...)
 	return nil
+}
+
+func (tds *todos) toogle(id int) error {
+	t := *tds
+	if err := t.validate_index(id); err != nil {
+		return err
+	}
+
+	is_completed := t[id].completed
+
+	if !is_completed {
+		completion_time := time.Now()
+		t[id].completed_at = &completion_time
+	}
+	t[id].completed = !is_completed
+	return nil
+}
+
+func (tds *todos) update(id int, new_title string) error {
+	t := *tds
+	if err := t.validate_index(id); err != nil {
+		return err
+	}
+
+	t[id].title = new_title
+	return nil
+}
+
+func (tds *todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "Title", "Completed", "Created At", "Completed At")
+	for id, t := range *tds {
+		completed := "❌"
+		completed_at := ""
+
+		if t.completed {
+			completed = "✅"
+			if t.completed_at != nil {
+				completed_at = t.completed_at.Format(time.RFC1123)
+			}
+		}
+
+		table.AddRow(strconv.Itoa(id), t.title, completed, t.created_at.Format(time.RFC1123), completed_at)
+	}
+	table.Render()
 }
